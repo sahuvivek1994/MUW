abd47df70e5d5494d117d980e1127fd7bab67775
sahuvivek1994 <vivek.sahu@inscripts.com>
2018-11-22 16:17:08 +0530

    update

---
+++ app/src/main/java/com/inscripts/ins_armman/npdsf/incompleteFormList/IncompleteFormListInteractor.java
@@ -0,0 +1,28 @@
+package com.inscripts.ins_armman.npdsf.incompleteFormList;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import com.inscripts.ins_armman.npdsf.database.DBHelper;
+
+public class IncompleteFormListInteractor implements IIncompleteFormListInteractor {
+
+    Context mContext;
+    DBHelper db;
+
+    public IncompleteFormListInteractor(Context mContext) {
+        this.mContext = mContext;
+        this.db = new DBHelper(mContext);
+    }
+
+    @Override
+    public Cursor getCompleteFormList(String unique_id) {
+        return db.getIncompleteFormList(unique_id);
+    }
+
+    @Override
+    public Cursor getChildNo(String unique_mother_id) {
+        return db.getChildIdFromMotherId(unique_mother_id);
+    }
+
+}
